4e55c39bc0c08f6dde907a2a799bc4a4dc7ca93a
corradt <tcorrado82@gmail.com>
2015-04-29 14:37:50 +0200

    complete app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :tasks do 
+    member do
+      put :change
+    end
+  end
+
   devise_for :users
   get 'pages/home'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304211745) do
+ActiveRecord::Schema.define(version: 20150305231206) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "state",        default: "to_do"
+    t.integer  "user"
+    t.integer  "user_id"
+    t.text     "content_html"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
